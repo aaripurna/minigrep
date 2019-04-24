fce2187bdb1d6c8105b83170fd24abd764bfc713
nawa aripurna <nap.aripurna@gmail.com>
2019-04-24 08:04:13 +0800

    improving IO

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,22 @@ pub struct Config {
 }
 
 impl Config {
-	pub fn new(args: &[String]) -> Result<Config, &'static str> {
+	pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
 
-		if args.len() < 3 {
-			return Err("Not enough arguments");
-		}
+		args.next();
+		let query = match args.next() {
+			Some(arg) => arg,
+			None => return Err("Didn't get a query string")
+		};
 
-		let query = args[1].clone();
-		let filename = args[2].clone();
-		let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+		let filename = match args.next() {
+			Some(arg) => arg,
+			None => return Err("Didn't get a file name")
+		};
 
-		Ok(Config {query, filename, case_sensitive})	
+		let case_sensitive = env::var("CASE_SENSITIVE").is_err();
+
+		Ok(Config {query, filename, case_sensitive})
 	}
 }
 
@@ -39,14 +44,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	let mut result = Vec::new();
-	for line in contents.lines() {
-		if line.contains(query) {
-			result.push(line);
-		}
-	}
-	result
-}
+	contents.lines()
+		.filter(|line| line.contains(query))
+		.collect()
+} 
+
 pub fn search_case_insensive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 	let query = query.to_lowercase();
 	let mut result = Vec::new();
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,8 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-
-	let config = Config::new(&args).unwrap_or_else(|err| {
-		eprintln!("Problem parsing arguments: {}", err);
+	let config = Config::new(env::args()).unwrap_or_else(|err| {
+		eprint!("Problem parsing elements: {}", err);
 		process::exit(1);
 	});
 
